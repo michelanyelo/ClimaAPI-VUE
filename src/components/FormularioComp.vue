358a9fa16debd612210ac175aadb9a1de7239190
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2024-12-20 15:44:19 -0300

    `feat: updated FormularioComp.vue with reactive busqueda object and added Chile to paises array`

--- src/components/FormularioComp.vue
+++ src/components/FormularioComp.vue
@@ -1,4 +1,12 @@
 <script setup>
+
+import { reactive } from 'vue';
+
+const busqueda = reactive({
+    ciudad: '',
+    pais: '',
+})
+
 const paises = [
     { codigo: 'US', nombre: 'Estados Unidos' },
     { codigo: 'MX', nombre: 'México' },
@@ -6,7 +14,8 @@ const paises = [
     { codigo: 'CO', nombre: 'Colombia' },
     { codigo: 'CR', nombre: 'Costa Rica' },
     { codigo: 'ES', nombre: 'España' },
-    { codigo: 'PE', nombre: 'Perú' }
+    { codigo: 'PE', nombre: 'Perú' },
+    { codigo: 'CL', nombre: 'Chile' }
 ]
 </script>
 
@@ -14,12 +23,12 @@ const paises = [
     <form class="formulario">
         <div class="campo">
             <label for="ciudad">Ciudad</label>
-            <input type="text" id="ciudad">
+            <input type="text" id="ciudad" placeholder="Ciudad" v-model="busqueda.ciudad">
         </div>
 
         <div class="campo">
             <label for="pais">Pais</label>
-            <select id="pais">
+            <select id="pais" v-model="busqueda.pais">
                 <option value="">-- Seleccione un país --</option>
                 <option v-for="pais in paises" :value="pais.codigo">{{ pais.nombre }}</option>
             </select>
