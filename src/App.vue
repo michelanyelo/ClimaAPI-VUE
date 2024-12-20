10a6eea43929b748d673174bd8438653d2bda4b8
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2024-12-20 15:40:18 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import FormularioComp from './components/FormularioComp.vue';
+</script>
+
+<template>
+  <h1 class="titulo">Buscador de CLima</h1>
+
+  <div class="contenedor buscador-clima">
+    <FormularioComp />
+  </div>
+
+</template>
+
+<style scoped></style>
---
+++ src/components/FormularioComp.vue
@@ -0,0 +1,32 @@
+<script setup>
+const paises = [
+    { codigo: 'US', nombre: 'Estados Unidos' },
+    { codigo: 'MX', nombre: 'México' },
+    { codigo: 'AR', nombre: 'Argentina' },
+    { codigo: 'CO', nombre: 'Colombia' },
+    { codigo: 'CR', nombre: 'Costa Rica' },
+    { codigo: 'ES', nombre: 'España' },
+    { codigo: 'PE', nombre: 'Perú' }
+]
+</script>
+
+<template>
+    <form class="formulario">
+        <div class="campo">
+            <label for="ciudad">Ciudad</label>
+            <input type="text" id="ciudad">
+        </div>
+
+        <div class="campo">
+            <label for="pais">Pais</label>
+            <select id="pais">
+                <option value="">-- Seleccione un país --</option>
+                <option v-for="pais in paises" :value="pais.codigo">{{ pais.nombre }}</option>
+            </select>
+        </div>
+
+        <input type="submit" value="Consultar Clima">
+    </form>
+</template>
+
+<style scoped></style>
