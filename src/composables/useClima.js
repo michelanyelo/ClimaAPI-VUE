fa6f20a1135eb97faebd3d0f3bcfdbb70ec28955
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2024-12-23 16:47:28 -0300

    `Added .env to .gitignore and modified useClima.js to import VITE_API_KEY`

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,6 +1,12 @@
 export default function useClima () {
     const obtenerClima = ({ciudad, pais}) => {
-        console.log("Consultando...", ciudad, pais);
+        // Importar el api
+        const key = import.meta.env.VITE_API_KEY;
+
+        // Obtener la lat, lon
+        
+
+        // Obtener el clima
     }
 
     return{
