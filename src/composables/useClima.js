1621ba1cbaa2c1c444549d84182e78c82e3717b4
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2024-12-23 16:50:44 -0300

    `Added axios and proxy-from-env dependencies, updated useClima composable to use OpenWeatherMap API`

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,15 +1,16 @@
-export default function useClima () {
-    const obtenerClima = ({ciudad, pais}) => {
+export default function useClima() {
+    const obtenerClima = ({ ciudad, pais }) => {
         // Importar el api
         const key = import.meta.env.VITE_API_KEY;
 
         // Obtener la lat, lon
-        
+        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+        console.log(url)
 
         // Obtener el clima
     }
 
-    return{
+    return {
         obtenerClima
     }
 }
