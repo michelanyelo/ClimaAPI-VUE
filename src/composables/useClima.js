2fdd0d0406fc41ef13214dda3c489780f774d40c
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2024-12-23 16:56:39 -0300

    `feat: updated useClima composable to use axios for API requests`

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,13 +1,22 @@
+import axios from "axios";
+
 export default function useClima() {
-    const obtenerClima = ({ ciudad, pais }) => {
+    const obtenerClima = async ({ ciudad, pais }) => {
         // Importar el api
         const key = import.meta.env.VITE_API_KEY;
 
-        // Obtener la lat, lon
-        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
-        console.log(url)
+        try {
+            // Obtener la lat, lon con axios
+            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+            const { data } = await axios.get(url);
+            const { lat, lon } = data[0];
+            console.log(lat, lon);
 
-        // Obtener el clima
+            // Obtener el clima con axios
+
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return {
