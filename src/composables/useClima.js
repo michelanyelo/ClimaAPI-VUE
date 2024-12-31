328b65b1d4e3f42793de8275bae8670586de27e8
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2024-12-31 16:19:54 -0300

    `Update useClima.js to use HTTPS URL for OpenWeatherMap API`

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -19,7 +19,7 @@ export default function useClima() {
 
         try {
             // Obtener la lat, lon con axios
-            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+            const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
             const { data } = await axios.get(url);
             const { lat, lon } = data[0];
 
